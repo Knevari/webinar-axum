16b1eb2bc1f3a53cf2eb45ae18e148e262c9d70f
Knevari <winresource000@gmail.com>
2024-04-18 13:14:26 -0300

    add rudimentary cache

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,58 @@
+//! Provides SQLX integration for the test database.
+//!
+//! The database is assumed to be in-memory, and rebuilt from
+//! scratch on each start-up.
+
 use anyhow::Result;
+use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, Row, SqlitePool};
+use tokio::sync::RwLock;
 
+/// Represents a book, taken from the books table in SQLite.
+#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
+pub struct Book {
+    /// The book's primary key ID
+    pub id: i32,
+    /// The book's title
+    pub title: String,
+    /// The book's author (surname, lastname - not enforced)
+    pub author: String,
+}
+
+struct BookCache {
+    all_books: RwLock<Option<Vec<Book>>>,
+}
+
+impl BookCache {
+    fn new() -> Self {
+        Self {
+            all_books: RwLock::new(None),
+        }
+    }
+
+    async fn all_books(&self) -> Option<Vec<Book>> {
+        let lock = self.all_books.read().await;
+        lock.clone()
+    }
+
+    async fn refresh(&self, books: Vec<Book>) {
+        let mut lock = self.all_books.write().await;
+        *lock = Some(books);
+    }
+
+    async fn invalidate(&self) {
+        let mut lock = self.all_books.write().await;
+        *lock = None;
+    }
+}
+
+static CACHE: Lazy<BookCache> = Lazy::new(BookCache::new);
+
+/// Create a database connection pool. Run any migrations.
+///
+/// ## Returns
+/// * A ready-to-use connection pool.
 pub async fn init_db() -> Result<SqlitePool> {
     let database_url = std::env::var("DATABASE_URL")?;
     let connection_pool = SqlitePool::connect(&database_url).await?;
@@ -9,21 +60,30 @@ pub async fn init_db() -> Result<SqlitePool> {
     Ok(connection_pool)
 }
 
-#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
-pub struct Book {
-    pub id: i32,
-    pub title: String,
-    pub author: String,
-}
-
+/// Retrieves all books, sorted by title and then author.
+///
+/// ## Arguments
+/// * `connection_pool` - the connection pool to use.
+///
+/// ## Returns
+/// * A vector of books, or an error.
 pub async fn all_books(connection_pool: &SqlitePool) -> Result<Vec<Book>> {
-    Ok(
-        sqlx::query_as::<_, Book>("SELECT * FROM books ORDER BY title,author")
+    if let Some(all_books) = CACHE.all_books().await {
+        Ok(all_books)
+    } else {
+        let books = sqlx::query_as::<_, Book>("SELECT * FROM books ORDER BY title,author")
             .fetch_all(connection_pool)
-            .await?,
-    )
+            .await?;
+        CACHE.refresh(books.clone()).await;
+        Ok(books)
+    }
 }
 
+/// Retrieves a single book, by ID
+///
+/// ## Arguments
+/// * `connection_pool` - the database connection pool to use
+/// * `id` - the primary key of the book to retrieve
 pub async fn book_by_id(connection_pool: &SqlitePool, id: i32) -> Result<Book> {
     Ok(sqlx::query_as::<_, Book>("SELECT * FROM books WHERE id=$1")
         .bind(id)
@@ -31,6 +91,15 @@ pub async fn book_by_id(connection_pool: &SqlitePool, id: i32) -> Result<Book> {
         .await?)
 }
 
+/// Adds a book to the database.
+///
+/// ## Arguments
+/// * `connection_pool` - the database connection to use
+/// * `title` - the title of the book to add
+/// * `author` - the author of the book to add
+///
+/// ## Returns
+/// * The primary key value of the new book
 pub async fn add_book<S: ToString>(
     connection_pool: &SqlitePool,
     title: S,
@@ -38,16 +107,22 @@ pub async fn add_book<S: ToString>(
 ) -> Result<i32> {
     let title = title.to_string();
     let author = author.to_string();
-    Ok(
-        sqlx::query("INSERT INTO books (title, author) VALUES ($1, $2) RETURNING id")
-            .bind(title)
-            .bind(author)
-            .fetch_one(connection_pool)
-            .await?
-            .get(0),
-    )
+    let id = sqlx::query("INSERT INTO books (title, author) VALUES ($1, $2) RETURNING id")
+        .bind(title)
+        .bind(author)
+        .fetch_one(connection_pool)
+        .await?
+        .get(0);
+    CACHE.invalidate().await;
+    Ok(id)
 }
 
+/// Update a book
+///
+/// ## Arguments
+/// * `connection_pool` - the database connection to use
+/// * `book` - the book object to update. The primary key will be used to
+///            determine which row is updated.
 pub async fn update_book(connection_pool: &SqlitePool, book: &Book) -> Result<()> {
     sqlx::query("UPDATE books SET title=$1, author=$2 WHERE id=$3")
         .bind(&book.title)
@@ -55,15 +130,21 @@ pub async fn update_book(connection_pool: &SqlitePool, book: &Book) -> Result<()
         .bind(&book.id)
         .execute(connection_pool)
         .await?;
-
+    CACHE.invalidate().await;
     Ok(())
 }
 
+/// Delete a book
+///
+/// ## Arguments
+/// * `connection_pool` - the database connection to use
+/// * `id` - the primary key of the book to delete
 pub async fn delete_book(connection_pool: &SqlitePool, id: i32) -> Result<()> {
     sqlx::query("DELETE FROM books WHERE id=$1")
         .bind(id)
         .execute(connection_pool)
         .await?;
+    CACHE.invalidate().await;
     Ok(())
 }
 
@@ -84,7 +165,6 @@ mod test {
         dotenv::dotenv().ok();
         let cnn = init_db().await.unwrap();
         let book = book_by_id(&cnn, 1).await.unwrap();
-
         assert_eq!(1, book.id);
         assert_eq!("Hands-on Rust", book.title);
         assert_eq!("Wolverson, Herbert", book.author);
@@ -101,6 +181,7 @@ mod test {
         assert_eq!("Test Author", new_book.author);
     }
 
+    #[sqlx::test]
     async fn test_update() {
         dotenv::dotenv().ok();
         let cnn = init_db().await.unwrap();
@@ -108,9 +189,10 @@ mod test {
         book.title = "Updated Book".to_string();
         update_book(&cnn, &book).await.unwrap();
         let updated_book = book_by_id(&cnn, 2).await.unwrap();
-        assert_eq!("Updateed Book", updated_book.title);
+        assert_eq!("Updated Book", updated_book.title);
     }
 
+    #[sqlx::test]
     async fn test_delete() {
         dotenv::dotenv().ok();
         let cnn = init_db().await.unwrap();
@@ -118,6 +200,6 @@ mod test {
         let _new_book = book_by_id(&cnn, new_id).await.unwrap();
         delete_book(&cnn, new_id).await.unwrap();
         let all_books = all_books(&cnn).await.unwrap();
-        assert!(all_books.iter().find(|b| b.title == "DeleteMe").is_none())
+        assert!(all_books.iter().find(|b| b.title == "DeleteMe").is_none());
     }
 }
