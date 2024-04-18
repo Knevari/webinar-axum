361e25afa7da5b8196d2353349b71f5cd0a2a6d7
Knevari <winresource000@gmail.com>
2024-04-18 12:33:23 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+mod db;
+mod rest;
+
+use std::net::SocketAddr;
+
+use crate::db::init_db;
+use anyhow::Result;
+use axum::{Extension, Router};
+use sqlx::SqlitePool;
+
+fn router(connection_pool: SqlitePool) -> Router {
+    Router::new()
+        .nest_service("/books", rest::books_service())
+        .layer(Extension(connection_pool))
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Load environment variables from .env if available
+    dotenv::dotenv().ok();
+
+    // Initialize the database and obtain a connection pool
+    let connection_pool = init_db().await?;
+
+    let app = router(connection_pool);
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
+}
